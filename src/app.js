f0e4ec309dd77bbe70f4db2bd49a65207af2cd93
bonnah joohn <bonnahpoku@gmail.com>
2023-02-01 16:11:49 +0000

    navBar scroll bgColor change

--- src/app.js
+++ src/app.js
@@ -4,3 +4,9 @@ const toggling = document.getElementById("toggle");
 button.addEventListener("click", () => {
   toggling.classList.toggle("left-0");
 });
+
+// nav bgColor change
+window.addEventListener("scroll", () => {
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("change", scrollY > 0);
+});
