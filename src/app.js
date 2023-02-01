c5de6133f7b3c4b0f3d0af0442df0396ef7081ca
bonnah joohn <bonnahpoku@gmail.com>
2023-02-01 00:52:23 +0000

    A website announcing plumbers work. NavBar has buid also

---
+++ src/app.js
@@ -0,0 +1,6 @@
+// navbar toggling
+const button = document.getElementById("togger");
+const toggling = document.getElementById("toggle");
+button.addEventListener("click", () => {
+  toggling.classList.toggle("left-0");
+});
